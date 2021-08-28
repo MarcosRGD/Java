b9efe4b59d97806712839a14f7b78e31ee71a982
Tr1sk33l <mastermind51@gmail.com>
2021-08-28 11:55:38 -0300

    Create HelloWorld.java
    comments

---
+++ HelloWorld.java
@@ -0,0 +1,28 @@
+package com.itbulls.gomez;
+
+/**
+ * Hello World Application
+ * 
+ * @author Hulk
+ *
+ */
+
+public class HelloWorld {
+
+	/**
+	 * comments
+	 * @param args
+	 */
+	public static void main(String[] args) {
+        /*
+         * comment
+         * 
+         */
+		
+		System.out.println("Hello World!");
+        
+        //comment
+        
+	}
+
+}
